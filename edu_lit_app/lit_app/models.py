c0104687b069b871f8629fd7851bc766cd19984d
Palakh Mignonne Jude <palakhjude@gmail.com>
2021-04-30 17:20:35 -0400

    fix minor issues with models (#6)
    Co-authored-by: Palakh Mignonne Jude <jpalakhmignonne@cs.vt.edu>

--- edu_lit_app/lit_app/models.py
+++ edu_lit_app/lit_app/models.py
@@ -14,17 +14,7 @@ class Question(models.Model):
     id = models.BigAutoField(primary_key=True)
     difficulty = models.IntegerField()
     answer_blob = models.TextField()
-    test = models.ForeignKey(Test, on_delete=models.CASCADE))
-
-class Answer(models.Model):
-    id = models.BigAutoField(primary_key=True)
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    student = models.ForeignKey(student, on_delete=models.CASCADE)
-    content = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    test_submission = models.ForeignKey(TestSubmission, on_delete=models.CASCADE)
 
 class TestSubmission(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -33,3 +23,13 @@ class TestSubmission(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    content = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    test_submission = models.ForeignKey(TestSubmission, on_delete=models.CASCADE)
